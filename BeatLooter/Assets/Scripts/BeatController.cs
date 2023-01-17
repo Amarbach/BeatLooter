dfc236888a23cbbb92762e6f31b83b4e4f646850
Amarbach <amarbach@interia.pl>
2023-01-17 19:02:09 +0100

    Combo and Combo Multiplier

--- BeatLooter/Assets/Scripts/BeatController.cs
+++ BeatLooter/Assets/Scripts/BeatController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Timers;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,15 +14,16 @@ public class BeatController : MonoBehaviour
     private Stopwatch beatEvaluator;
     private Stopwatch sinceLast;
     private float errorMargin = 0.3f;
-    public int testBeat = 0;
+    private int comboCount = 0;
+    private bool wasHit = false;
     [SerializeField] private Slider rightBar;
     [SerializeField] private Slider leftBar;
+    [SerializeField] private TextMeshProUGUI comboText;
+    public float ComboMultiplier { get { if (comboCount < 3) return 1f; else if (comboCount < 6) return 1.5f; else return 2f; } }
 
     public AudioSource audioPlayer;
     public AudioClip beatSound;
     public AudioClip hitSound;
-
-    public ActionController[] enemies = { null, null, null };
 
     public ActionController playerCharacter;
     // Start is called before the first frame update
@@ -39,46 +42,73 @@ public class BeatController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             float eval = EvaluateHit();
-            float sinceLastClick = sinceLast.ElapsedMilliseconds;
-            UnityEngine.Debug.Log(eval);
-            if (eval < errorMargin && sinceLastClick > 300) playerCharacter.initiateMove(new Vector3(1, 0, 0));
+            //UnityEngine.Debug.Log(eval);
+            if (eval < errorMargin)
+            {
+                Combo();
+                playerCharacter.initiateMove(new Vector3(1, 0, 0));
+                wasHit = true;
+            }
             sinceLast.Restart();
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A))
         {
             float eval = EvaluateHit();
-            float sinceLastClick = sinceLast.ElapsedMilliseconds;
-            UnityEngine.Debug.Log(eval);
-            if (eval < errorMargin && sinceLastClick > 300) playerCharacter.initiateMove(new Vector3(-1, 0, 0));
+            //UnityEngine.Debug.Log(eval);
+            if (eval < errorMargin)
+            {
+                Combo();
+                playerCharacter.initiateMove(new Vector3(-1, 0, 0));
+                wasHit = true;
+            }
             sinceLast.Restart();
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        else if (Input.GetKeyDown(KeyCode.W))
         {
             float eval = EvaluateHit();
-            float sinceLastClick = sinceLast.ElapsedMilliseconds;
-            UnityEngine.Debug.Log(eval);
-            if (eval < errorMargin && sinceLastClick > 300) playerCharacter.initiateMove(new Vector3(0, 1, 0));
+            //UnityEngine.Debug.Log(eval);
+            if (eval < errorMargin)
+            {
+                Combo();
+                playerCharacter.initiateMove(new Vector3(0, 1, 0));
+                wasHit = true;
+            }
             sinceLast.Restart();
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S))
         {
             float eval = EvaluateHit();
-            float sinceLastClick = sinceLast.ElapsedMilliseconds;
-            UnityEngine.Debug.Log(eval);
-            if (eval < errorMargin && sinceLastClick > 300) playerCharacter.initiateMove(new Vector3(0, -1, 0));
+            //UnityEngine.Debug.Log(eval);
+            if (eval < errorMargin)
+            {
+                Combo();
+                playerCharacter.initiateMove(new Vector3(0, -1, 0));
+                wasHit = true;
+            }
             sinceLast.Restart();
         }
+        //if (beatEvaluator.ElapsedMilliseconds > errorMargin)
+        //{
+        //    if (!wasHit)
+        //    {
+        //        MissBeat();
+        //    }
+        //    wasHit = false;
+        //}
+
     }
 
     private void OnBeat()
     {
         beatEvaluator.Restart();
-        testBeat += 1;
+        StartCoroutine(CheckMiss());
     }
 
     private float EvaluateHit()
     {
         float hit = beatEvaluator.ElapsedMilliseconds;
+        float sinceLastClick = sinceLast.ElapsedMilliseconds;
+        if (sinceLastClick < beatInterval / 6f) return 1.0f;
         if (hit > beatInterval / 2f * 1000f)
         {
             return (((float)beatInterval * 1000f) - hit) / ((float)beatInterval / 2f * 1000f);
@@ -97,5 +127,32 @@ public class BeatController : MonoBehaviour
             yield return new WaitForSeconds(beatInterval);
             OnBeat();
         }
+    }
+
+    private IEnumerator CheckMiss()
+    {
+        yield return new WaitForSeconds(errorMargin);
+        if (!wasHit) MissBeat();
+        else wasHit = false;
+    }
+
+    private void MissBeat()
+    {
+        ResetCombo();
+    }
+
+    private void Combo()
+    {
+        comboCount++;
+        comboText.text = "Multiplier:" + ComboMultiplier;
+        if (comboCount >= 6) comboText.color = Color.red;
+        else comboText.color = Color.white;
+    }
+
+    private void ResetCombo()
+    {
+        comboCount = 0;
+        comboText.text = "Multiplier:" + ComboMultiplier;
+        comboText.color = Color.white;
     }
 }
